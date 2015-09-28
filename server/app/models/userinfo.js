d74ec71deec059df995f8e187c535c55f740f3cd
Nate Brady <nathan.brady@yahoo.com>
2015-09-28 11:15:47 -0700

    Fixed bug in UserInfo model

--- server/app/models/userinfo.js
+++ server/app/models/userinfo.js
@@ -4,18 +4,18 @@
 var mongoose = require('mongoose');
 
 var UserInfoSchema = new mongoose.Schema({
-	active_rooms: [
-		{type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
+	active_rooms: [{
+		_room: {type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
 		upvotes: Number,
 		downvotes: Number,
 		message_count: Number
-	],
-	inactive_rooms: [
-		{type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
+	}],
+	inactive_rooms: [{
+		_room: {type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
 		upvotes: Number,
 		downvotes: Number,
 		message_count: Number
-	]
+	}]
 });
 
 mongoose.model('UserInfo', UserInfoSchema);
