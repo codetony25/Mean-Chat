edaef8a7eac8b5d9f2807bb92908ef121736b359
ahnkee <andrew.a.lee@gmail.com>
2015-09-30 02:21:08 -0700

    Removed console logs

--- public/app/components/dashboard/dashboard-controller.js
+++ public/app/components/dashboard/dashboard-controller.js
@@ -13,16 +13,13 @@
 
         this.getUserInfo = function() {
             DashboardFactory.fetchUserInfo( function(response) {
-
                 if(response.state == 'success') {
                     _this.userInfo = response.user;
-                    console.log(_this.userInfo);
                 }
             })
         }
 
         var _init = function _init() {
-            console.log('Init Run');
             _this.getUserInfo();
         }
 
