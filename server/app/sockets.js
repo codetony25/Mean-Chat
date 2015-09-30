d4ceaaee6d14ba35d184bfb49c939431b59df9d2
Nate Brady <nathan.brady@yahoo.com>
2015-09-30 08:24:50 -0700

    Changed socket emitters to send message object

--- server/app/sockets.js
+++ server/app/sockets.js
@@ -35,29 +35,38 @@ module.exports.listen = function(app){
 
     io.on('connection', function(socket) {
         var userId = socket.request.session.passport.user;
-        //socket.request.session.passport.user <--user id
 
+        /**
+        * Recieving a new message from the client
+        */
         socket.on('new_message', function(data) {
+            // Encode the message to prevent xss/script injection
             var msg = entities.encode(data.message);
 
             console.log(userId);
-            
-            msg = msg.replace('[b]','<b>');
-            msg = msg.replace('[/b]','</b>');
 
-            message = new Message({
+            // After encoding check for BBCode markup
+            msg = msg.replace('[b]','<b>').replace('[/b]','</b>');
+            msg = msg.replace('[i]','<i>').replace('[/i]','</i>');
+            msg = msg.replace('[u]','<u>').replace('[/u]','</u>');
+            msg = msg.replace('[s]','<s>').replace('[/s]','</s>');
+
+            // Create the message object to save
+            var message = new Message({
                 _owner: userId,
-                _room: data.roomId,
+                _room: data._room,
+                time: Date.now(),
                 message: msg,
-                resource_type: data.resourceType
+                resource_type: data.resource_type
             });
 
+            // Attempt to save the message
             message.save(function(err) {
                 if (!err) {
                     // If there are no errors, emit the message to the room
-                    io.emit('room_' + data.roomId, {message: msg});
+                    io.emit('room_' + data._room, message);
                     // Emit an event to those that have the room docked
-                    io.emit('docked_' + data.roomId);
+                    io.emit('docked_' + data._room);
                     // Once we've emitted to the room, update the users message count
                     User.update({_id: userId}, { $inc: {message_count: 1}, last_activity: Date.now() }, function(err) { 
                         console.log('here');
@@ -74,18 +83,50 @@ module.exports.listen = function(app){
         });
 
         socket.on('join_room', function(data) {
-            Room.findOne({_id: data.roomId}, function(err, room) {
+            console.log(data);
+            Room.findOne({_id: data._room}, function(err, room) {
                 // Make sure the user isn't blocked
-                if (room._blocked.indexOf(userId) > -1) {
-                    User.findOne({_id: userId}, function(err, userInfo) {
-                        // If the room is in userInfo.inactive_rooms, move it to active_rooms
+
+                if (room._blocked.indexOf(userId) == -1) {
+                    User.findOne({_id: userId}, function(err, userInfo) {                        
+                        // If the room doesn't already exist as an active room, make it an active room
+                        User.update({_id: userId}, {$addToSet: { active_rooms: data._room }}, function(err) { console.log('got here')});
+                        // Emit to the user so that a dynamic socket can be created
+                        socket.emit('joined_room', {_room: data._room});
+                        // Create a new system message to send to the room
+                        var message = new Message({
+                            _owner: 'System',
+                            _room: data._room,
+                            resource_type: 'System',
+                            time: Date.now(),
+                            message: userInfo.username + ' has joined the room.'
+                        });
+                        // Attempt to save the message
+                        message.save(function(err) {
+                            if (!err) {
+                                // If there are no errors, emit the message to the room
+                                io.emit('room_' + data._room, message);
+                                // Emit an event to those that have the room docked
+                                io.emit('docked_' + data._room);
+                                // Remove the room from the recently visited arrays if it exits and then push it
+                                // Last in, First out in order of most recently visited
+                                User.update({_id: userId}, {$pull: { recent_rooms: data._room }, $push: { recent_rooms: data._room }}, function(err) { console.log(err); });
+                            } else {
+                                //There was an error saving the message for some reason
+                                // Probably shouldn't display it to the room
+                                console.log(err);
+                            }
+                        });
                     });                    
                 } else {
-                    // The user is blocked
+                    // The user is blocked, emit some failure back
                 }
             });
         });
 
+        socket.on('msg_vote', function(data) {
+
+        });
 
     });
 
